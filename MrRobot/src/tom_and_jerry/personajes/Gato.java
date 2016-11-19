65931c39bb7353b471b2615a08d2f16ae4893941
Andrey Pérez <robin.perez@ucr.ac.cr>
2016-11-19 11:58:56 -0600

    Implementación tentativa
    Se implementó un código preliminar para controlar tanto al gato como al
    ratón. Ahora estamos en fase de pruebas

--- MrRobot/src/tom_and_jerry/personajes/Gato.java
+++ MrRobot/src/tom_and_jerry/personajes/Gato.java
@@ -1,20 +1,35 @@
 package tom_and_jerry.personajes;
 
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
 
-import lejos.hardware.motor.Motor;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.robotics.chassis.Chassis;
-import lejos.robotics.chassis.Wheel;
-import lejos.robotics.chassis.WheeledChassis;
-import lejos.robotics.navigation.MovePilot;
+public class Gato extends Jugador {
+	private SensorModes ojos;
+	private static final Port PUERTO = LocalEV3.get().getPort("S2");
+	private static final int MINIMA_DISTANCIA = 10;
+	
+	
+	public Gato() {
+		super();
+		
+		ojos = new EV3UltrasonicSensor(PUERTO);
+	}
 
-public class Gato implements Jugador {
-    public Gato() {
-    }
+	@Override
+	protected boolean heGanado() {
+		SampleProvider  distancia = ojos.getMode("Distance");
+		float[] sample = new float[distancia.sampleSize()];
+		distancia.fetchSample(sample, 0);
+		
+		return (sample[0] < MINIMA_DISTANCIA);
+	}
 
-    @Override
-    public boolean jugar() {
-        return false;
-    }
+	@Override
+	protected boolean hePerdido() {
+		// TODO Comunicarse con Raton
+		return false;
+	}
 }
